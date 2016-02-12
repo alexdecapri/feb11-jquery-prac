47dc6a0d98ab823cb733ec2cc2a6cb9d562a3e11
alexdecapri <alexdecapri@gmail.com>
2016-02-11 18:50:39 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+   
+	// var color = "green";
+
+	$(".main").on("click", function() {
+		$(this).toggleClass("green"); //when you add classes, "." isn't needed
+	});
+
+
+
+
+
+});
